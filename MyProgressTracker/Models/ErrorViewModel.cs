29b5180f273e0e7c849cc1b247709e178b72298c
Ayesh Perera <givanthaperera25@gmail.com>
2024-06-29 15:29:37 +0530

    init

---
+++ MyProgressTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyProgressTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# MyProgressTracker
